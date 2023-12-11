c872fb160ca8d763d65d034494e1b08a7d723387
Jaalke <adamski.jan.franciszek@gmail.com>
2023-12-11 12:53:13 +0100

    Wrote a function to replace all occurrences of a string (i hate c)

--- 01/digit.c
+++ 01/digit.c
@@ -2,21 +2,34 @@
 #include <stdlib.h>
 #include <string.h>
 
-void word_to_digit(char *og_string, char *word, char digit) {
+char *replace_all_words(char *input_string, const char *target, char *replacement) {
+    size_t word_len = strlen(target);
+    size_t string_len = strlen(input_string);
+
+    char org_string[string_len];
+    strcpy(org_string, input_string);
+    input_string[0] = '\0';
+
+    char *segment_start = org_string;
+    char *segment_end = org_string;
     
+    while (strstr(segment_start, target) != NULL) {
+        segment_end = strstr(segment_start, target);
+        strncat(input_string, segment_start, segment_end - segment_start);
+        strcat(input_string, replacement);
+        segment_end += word_len;
+        segment_start = segment_end;
+    }
 
-    size_t word_len = strlen(word);
-    size_t string_len = strlen(og_string);
+    strcat(input_string, segment_start);
 
-
+    return input_string;
 }
 
 int main(int argc, int **argv) {
-    char *string = "aewrgaertdvsevenasdfqer2";
+    char string[64] = "sevenpartonesevensevenparttwosevenpartthreeseven";
     char *needle = "seven";
-    char *ouputstring = strstr(string, needle);
-    
-    if (ouputstring != NULL) {
-        printf("%p\n", ouputstring - string);
-    }
+    char *digit = "7";
+
+    printf("%s\n", replace_all_words(string, needle, digit));
 }
