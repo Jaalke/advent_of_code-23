caa4e3c1b24890c39732d3440cc16344f61bfde1
Jaalke <92577869+Jaalke@users.noreply.github.com>
2024-05-09 15:54:12 +0200

    Almost there on part two

--- 04/part_two.pl
+++ 04/part_two.pl
@@ -24,10 +24,46 @@ cards_from_points(CardNumber, Points, [NextCardNumber|Rest]) :-
 
 % Predicate for finding cards won from a card
 
-cards_won_from_card(Card, WonCards) :-
+numbers_won_from_card(Card, WonNumbers) :-
     card_value(Card, Value),
     card_number(Card, Number),
-    cards_from_points(Number, Value, WonCards).
+    cards_from_points(Number, Value, WonNumbers).
+
+get_card_by_number(Card, Cards, Number) :- 
+    member(Card, Cards),
+    card_number(Card, Number).
+
+numbers_from_cards([], []).
+
+numbers_from_cards([CurrentCard|RestCards], [CurrentNumber|RestNumbers]) :-
+    numbers_from_cards(RestCards, RestNumbers),
+    card_number(CurrentCard, CurrentNumber).
+
+numbers_won_from_number(Cards, Number, WonNumbers) :-
+    get_card_by_number(Card, Cards, Number),
+    numbers_won_from_card(Card, WonNumbers).
+
+numbers_won_from_numbers(_, [], []).
+
+numbers_won_from_numbers(Cards, [CurrentNumber|Rest], [CurrentWon|RestWon]) :-
+    numbers_won_from_numbers(Cards, Rest, RestWon),
+    numbers_won_from_number(Cards, CurrentNumber, CurrentWon).
+
+% Recursively getting all the scratchcards won
+
+scratchcards_won_from_numbers(0, [], _).
+
+scratchcards_won_from_numbers(NScratchcards, Numbers, Cards) :-
+    numbers_won_from_numbers(Cards, Numbers, WonNumbers),
+    scratchcards_won_from_numbers(NRest, WonNumbers, Cards),
+    length(WonNumbers, NCurrent),
+    NScratchcards is NCurrent + NRest.
+
+all_won_scratchcards(NTotal, Cards) :-
+    numbers_from_cards(Cards, Numbers),
+    scratchcards_won_from_numbers(N, Numbers, Cards),
+    NTotal is N + 220.
+
 
 %%%%%%%%%%%%
 %%% MAIN %%%
@@ -39,5 +75,5 @@ cards_won_from_card(Card, WonCards) :-
 
 main(_) :-
     get_lines_from_file(Lines, "input.txt"),
-    total_cards_value(Lines, Value),
-    format('~d ~n', Value).
+    all_won_scratchcards(Value, Lines),
+    format('~s ~n', [Value]).
